344dd15c35d4197da4c304d64cc1a080f953aaf1
Jupiter 3x3cu7er <jupiter3x3cu7er@gmail.com>
2024-07-21 11:23:03 -0400

    hosting

--- Portfolio/Portfolio/settings.py
+++ Portfolio/Portfolio/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import dj_database_url
+import os 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3tmf_^wtr^@vkwyx%p_s@aad(qar!y^t2#r=mztux+2g4*rz=m'
+# SECRET_KEY = 'django-insecure-3tmf_^wtr^@vkwyx%p_s@aad(qar!y^t2#r=mztux+2g4*rz=m'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG","False").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS","True").split(" ")
 
 
 # Application definition
@@ -82,6 +84,11 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+database_url = os.environ.get('DATABASE_URL')
+DATABASES["default"]=dj_database_url.parse(database_url)
+
+# "postgresql://portfoliodb_mosv_user:QwwNIVUW5e2s0PGYoUSeWdvdKRZKF5fO@dpg-cqeeobg8fa8c73e5fle0-a.oregon-postgres.render.com/portfoliodb_mosv"
 
 
 # Password validation
